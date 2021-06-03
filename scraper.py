4e2af6221e7a02429b56182ab23f415663a1966a
Andy Lulham <a.lulham@gmail.com>
2021-06-03 09:02:01 +0100

    Aha okay – fix scraper

--- scraper.py
+++ scraper.py
@@ -6,7 +6,11 @@ import requests
 import openpyxl
 
 # via https://fts.unocha.org/plan-code-list-iati
-r = requests.get('https://fts.unocha.org/download/126339/download')
+url = 'https://fts.unocha.org/download/initiate/views_executable/xlsx?uri=/plan-code-list-iati&query%5Buri%5D=/plan-code-list-iati&query%5Bview_id%5D=plan_code_list_for_iati&query%5Bview_display%5D=page&query%5B_wrapper_format%5D=drupal_modal&view_id=plan_code_list_for_iati&view_display=page'
+r = requests.get(url)
+download_id = r.json()[0].get('download_id')
+
+r = requests.get(f'https://fts.unocha.org/download/{download_id}/download')
 wb = openpyxl.load_workbook(BytesIO(r.content))
 sheet = wb['Export data']
 rows = [[cell.value for cell in row] for row in sheet.rows]
